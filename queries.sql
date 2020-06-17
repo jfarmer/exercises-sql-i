a6528f0d63d5c82f33f7b745d724b9a20ffe249b
Ben Santiago <besantiago@davidson.edu>
2020-06-17 19:54:32 -0400

    lots of guesswork

--- queries.sql
+++ queries.sql
@@ -15,6 +15,8 @@
 -- thinking the better feedback we'll be able to give!
 
 -- "albums" table
+SELECT albums;
+--I'm guessing
 
 -- Every album
 SELECT * FROM albums;
@@ -33,41 +35,63 @@ SELECT * FROM albums ORDER BY title DESC;
 SELECT * FROM albums WHERE title LIKE 'A%' ORDER BY title;
 
 -- "invoices" table
+SELECT invoices;
 -- Remember: run "\d+ invoices" to see the structure of the "invoices" table
 
 -- Every invoice
+SELECT * FROM invoices;
 
 -- Every invoice ordered by total invoice amount ("total")
+SELECT * FROM invoices ORDER BY total;
+-- SELECT total FROM invoices ORDER BY ?
+-- SELECT total would get all the totals
 
 -- Every invoice with a total greater than 10
+SELECT * FROM invoices WHERE total>10;
+-- WHERE is wrong?
 
 -- The 10 least expensive invoices
 -- Remember: ORDER BY orders from lowest-to-highest by default
 SELECT * FROM invoices ORDER BY total LIMIT 10;
 
 -- The 10 most expensive invoices
+SELECT * FROM invoices ORDER BY total DESC;
 
 -- The 15 most recent invoices
+SELECT * FROM invoices ORDER BY invoice_date LIMIT 15;
 
 -- The 15 oldest invoices
+SELECT * FROM invoices ORDER BY invoice_date DESC LIMIT 10;
 
 -- The 10 most expensive invoices from the US
+SELECT * FROM invoices ORDER BY billing_country, total DESC LIMIT 10 WHERE billing_country = 'USA';
+--sort by country then total
 
 -- The 10 least expensive invoices from the US
+SELECT * FROM invoices ORDER BY billing_country, total LIMIT 10 WHERE billing_country = 'USA';
+--SELECT * FROM invoices WHERE billing_country = 'US' ORDER BY total LIMIT 10;
+-- unsure which way is correct, but AM sure I dont know the correct order to place the queries
+
 
 -- The 10 most expensive invoices from outside the US
+SELECT * FROM invoices ORDER BY total WHERE billing_country != 'USA';
+-- I know I ordered this one differently than the above, im unsure which is correct but this one seems the best?
 -- Hint: If "=" means equal, use "!=" to mean "not equal"
+-- does <> work for not equal as well
 
 -- Every invoice from Chicago, IL
 SELECT * FROM invoices WHERE billing_city = 'Chicago' AND billing_state = 'IL' AND billing_country='USA';
 
 -- A list of all the invoices worth more than $5.00 from Chicago, IL
+SELECT * FROM invoices WHERE billing_city = 'Chicago' AND billing_state = 'IL' AND billing_country='USA' AND total>10;
 
 -- The billing addresses of the 5 most valuable invoices from Mountain View CA
 -- Gotta reward those big spenders!
+SELECT * FROM invoces ORDER BY total DESC LIMIT 5 WHERE billing_city = 'Mountain View' AND billing_state = 'CA';
 
 -- A list of the 10 most valuable invoices made before January 1st, 2010
 -- Hint: Dates are formatted like 'YYYY-MM-DD' and you can compare them using '<', '>', '<=' and '>='
+SELECT * FROM invoices ORDER BY total DESC LIMIT 10 WHERE invoice_date < 2010-01-01;
 
 
 -- The number of invoices from Chicago, IL
@@ -89,9 +113,14 @@ SELECT billing_state, COUNT(*), SUM(total) FROM invoices WHERE billing_country =
 SELECT billing_state, COUNT(*), SUM(total), AVG(total) FROM invoices WHERE billing_country = 'USA' AND billing_state = 'CA' GROUP BY billing_state;
 
 -- The count, total, and average of invoice totals, grouped by state, ordered by average invoice total from highest-to-lowest
+SELECT billing_state, COUNT(*), SUM(total), AVG(totals) FROM invoices WHERE billing_country = 'USA' GROUP BY billing_state ORDER BY AVG(total) DESC;
 
 -- A list of the top 5 countries by number of invoices
+SELECT billing_country, COUNT(*) FROM invoices GROUP BY billing_country ORDER BY COUNT(*) DESC LIMIT 5;
 
 -- A list of the top 5 countries by gross/total invoice size
+SELECT billing_country, SUM(total) FROM invoices GROUP BY billing_country ORDER BY SUM(total) DESC LIMIT 5;
+-- I dont really know what is meant by invoice size
 
 -- A list of the top 5 countries by average invoice size
+SELECT billing_country, AVG(total) FROM invoices GROUP BY billing_country ORDER BY AVG(total) DESC LIMIT 5;
